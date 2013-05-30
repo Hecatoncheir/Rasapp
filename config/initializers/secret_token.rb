58c5efc17f73fd875d1aaf33335c32c5d0cdb31a
Rasarts <rasart.pro@gmail.com>
2013-05-30 08:10:32 +0400

    Up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rasapp::Application.config.secret_key_base = 'df679ee32148c3c23abd7d5169d9de13054b017b5c97e309ef0d2e0d55986dc44c9c14dca11f5fdb3b1f2b95f9d5baeca9591f04ca1921158a0b49e28c17e119'
