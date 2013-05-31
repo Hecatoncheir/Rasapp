a29e6d9f7a3273c04296dc5eba16c6b6e245bac1
Rasarts <rasart.pro@gmail.com>
2013-05-31 10:05:14 +0400

    add template first_up

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(:default, Rails.env)
 
 module Rasapp
   class Application < Rails::Application
+
+    # config.assets.enabled = true
+    # config.assets.precompile += ['pages.js', 'pages.css']
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
