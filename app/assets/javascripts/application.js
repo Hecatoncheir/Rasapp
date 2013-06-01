fb948a02f95c847c4ca355a15723d27067c996bd
Rasarts <rasart.pro@gmail.com>
2013-06-01 13:27:52 +0400

    add some style for home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,16 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
+
+WebFontConfig = {
+    google: { families: [ 'Cuprum:400,700:latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })(); 
---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,4 @@
+$('.projects').masonry({
+	itemSelector : '.project',
+	columnWidth : 240
+});
